10a05d8377e07d4fbd3fd4d13cc0f9844b926850
Andrew Keller <Keller192@gmail.com>
2017-11-16 14:36:54 -0500

    Added hero and monster images

--- catch_the_monster.py
+++ catch_the_monster.py
@@ -1,15 +1,31 @@
 import pygame
 
+class Hero(object):
+    def __init__(self):
+        self.image = pygame.image.load('images/hero.png')
+
+    def draw(self, screen):
+        screen.blit(self.image, (240, 224))
+
+class Monster(object):
+    def __init__(self):
+        self.image = pygame.image.load('images/monster.png')
+    
+    def draw(self, screen):
+        screen.blit(self.image, (53, 50))
+    
 def main():
     width = 512
     height = 480
     pygame.init()
     screen = pygame.display.set_mode((width, height))
     background_image = pygame.image.load('images/background.png').convert_alpha()
-    pygame.display.set_caption('My Game')
+    pygame.display.set_caption('Catch the Monster!')
     clock = pygame.time.Clock()
 
     # Game initialization
+    hero = Hero()
+    monster = Monster()
 
     stop_game = False
     while not stop_game:
@@ -24,10 +40,12 @@ def main():
         # Game logic
 
         # Draw background
+
         screen.blit(background_image, (0, 0))
 
         # Game display
-
+        hero.draw(screen)
+        monster.draw(screen)
         pygame.display.update()
         clock.tick(60)
 
