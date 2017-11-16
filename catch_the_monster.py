f9c0fe9aad1d026e5b0aa4e7223078d341e96a67
Andrew Keller <Keller192@gmail.com>
2017-11-16 15:49:14 -0500

    Added random factor to monster movement, still need to finish

--- catch_the_monster.py
+++ catch_the_monster.py
@@ -1,4 +1,5 @@
 import pygame
+import random
 
 class Hero(object):
     def __init__(self):
@@ -12,7 +13,7 @@ class Monster(object):
         self.image = pygame.image.load('images/monster.png')
         self.x = 53
         self.y = 50
-        self.speed = 3
+        self.speed = 4 
     
     def draw(self, screen):
         screen.blit(self.image, (self.x, self.y))
@@ -31,6 +32,7 @@ def main():
     monster = Monster()
 
     stop_game = False
+    timer_count = 120
     while not stop_game:
         for event in pygame.event.get():
 
@@ -40,7 +42,14 @@ def main():
 
 
         # Game logic
-
+        if monster.x > 472:
+            monster.x = 20
+        if monster.x < 20:
+            monster.x = 472
+        if monster.y > 460:
+            monster.y = 20
+        if monster.y < 20:
+            monster.y = 460
         # Draw background
         screen.blit(background_image, (0, 0))
 
@@ -50,8 +59,28 @@ def main():
         pygame.display.update()
         
         # Monster movement
-        monster.x += monster.speed
         clock.tick(60)
+        timer_count = timer_count + 1
+        print timer_count
+        monster.x += monster.speed
+        monster.y += monster.speed
+        if timer_count > 120:
+            
+            timer_count = 0
+            random_direction = random.random()
+            
+            if random_direction < 0.25:
+                pass
+            elif 0.25 <= random_direction < 0.50:
+                monster.speed = -(monster.speed)
+            
+            elif 0.50 <= random_direction < 0.75:
+                pass
+            else:
+                monster.speed = -(monster.speed)
+
+
+
 
     pygame.quit()
 
