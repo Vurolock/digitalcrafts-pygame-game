9306198313c87d02a9aff3fc033ec80d6bbfd600
Andrew Keller <Keller192@gmail.com>
2017-11-17 11:38:42 -0500

    Added comments

--- catch_the_monster.py
+++ catch_the_monster.py
@@ -40,35 +40,38 @@ def main():
     # Game initialization
     hero = Hero()
     monster = Monster()
-
     stop_game = False
     timer_count = 119
+
     while not stop_game:
         for event in pygame.event.get():
 
             # Event handling
             if event.type == pygame.QUIT:
                 stop_game = True
-
-            if event.type == pygame.KEYDOWN:
-
-                if event.key == pygame.K_DOWN:
-                    
-                    hero.speed_y = hero.base_speed
-                elif event.key == pygame.K_UP:
-                    
-                    hero.speed_y = -hero.base_speed
-                elif event.key == pygame.K_LEFT:
-                    
-                    hero.speed_x = -hero.base_speed
-                elif event.key == pygame.K_RIGHT:
-                    
-                    hero.speed_x = hero.base_speed
+            
+            # If no keys are pressed hero will not move
             if event.type == pygame.KEYUP:
                 hero.speed_x = 0
                 hero.speed_y = 0
+            
+            # Key held in down position will move hero
+            if event.type == pygame.KEYDOWN:
+                print event.key
+                
+                if event.key == pygame.K_DOWN: #274
+                    hero.speed_y = hero.base_speed
+                
+                elif event.key == pygame.K_UP: #273
+                    hero.speed_y = -hero.base_speed
+                
+                elif event.key == pygame.K_LEFT: #276
+                    hero.speed_x = -hero.base_speed
+                
+                elif event.key == pygame.K_RIGHT: #275
+                    hero.speed_x = hero.base_speed
 
-        # Game logic
+        # Boundary warping
         if monster.x > 472:
             monster.x = 20
         if monster.x < 20:
@@ -77,7 +80,18 @@ def main():
             monster.y = 20
         if monster.y < 20:
             monster.y = 450
+        if hero.x > 472:
+            hero.x = 20
+        if hero.x < 20:
+            hero.x = 472
+        if hero.y > 450:
+            hero.y = 20
+        if hero.y < 20:
+            hero.y = 450
+        
+        # Updates position of hero every loop
         hero.update()
+        
         # Draw background
         screen.blit(background_image, (0, 0))
 
@@ -89,10 +103,10 @@ def main():
         # Monster movement
         clock.tick(60)
         timer_count = timer_count + 1
-        print timer_count
         monster.x += monster.speed_x
         monster.y += monster.speed_y
 
+        # Changes monster direction randomly every 2 seconds
         if timer_count >= 120:
             timer_count = 0
             random_direction = random.randint(0, 7)
